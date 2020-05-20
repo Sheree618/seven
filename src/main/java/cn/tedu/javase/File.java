5a3bc785853455575e5a52b3ad0d09a8330125ed
Sheree618 <1192539220@qq.com>
2020-05-20 17:18:24 +0800

    有用的

---
+++ src/main/java/cn/tedu/javase/File.java
@@ -0,0 +1,16 @@
+package cn.tedu.javase;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+public class File {
+    public static void main(String[] args){
+        FileInputStream fis = null;
+        GZIPInputStream gis = null;
+        InputStreamReader isr = null;
+        BufferedReader bufr = null;
+        String inputPath = "D:\\2020.2\\01-课前资料\\任务\\jobs1.csv";
+    }
+}
